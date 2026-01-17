8165c597a61b10dbe0d51b5a4d3b7574d1e2b8b8
mercurio123-hash <mercurio123@gmail.com>
2026-01-17 13:01:11 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'parquimetro-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './icon-parking.png',
+  './manifest.json'
+];
+
+// Instalación
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activación
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch (Para servir contenido offline si es necesario)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
